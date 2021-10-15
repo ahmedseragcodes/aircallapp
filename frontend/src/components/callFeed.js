e8a33a993c0ef081d43c2886d752bb29345e81ed
ahmedseragcodes <ahmedserag1@gmail.com>
2021-10-15 12:35:12 -0400

    setup mapStateToProps functions

--- frontend/src/components/callFeed.js
+++ frontend/src/components/callFeed.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { connect } from "react-redux";
 
 const CallFeed = () => {
 
@@ -13,4 +14,12 @@ const CallFeed = () => {
 
 }
 
-export default CallFeed;
+const mapStateToProps = (state) => {
+    return ({
+      allCalls: state.allCalls,
+      archivedCalls: state.archivedCalls,
+      error: state.error,
+    })
+  }
+  
+  export default connect(mapStateToProps, {})(CallFeed);
